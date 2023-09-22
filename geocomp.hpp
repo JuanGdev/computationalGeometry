a8fbf232937926bb9e27bceb6815f282b5f6918d
Juan Aguilera <73394216+JuanGdev@users.noreply.github.com>
2023-09-22 15:51:18 -0600

    Master (#2)
    * Start of repo
    
    * Excluding executables
    
    * Feature: Identify monotonal lines
    
    * Agregando archivos al .gitignore
    
    * Creando pruebas unitarias para is_monotone and area2
    
    * Adding functions to know if 3 points are in left or right
    
    * Remake of code
    
    * Unit test for intersection points
    
    * Passing x,y by reference with inter_pt
    
    * Agregando el calculo de indices circulares
    
    Implementacion de la función y pruebas unitarias
    
    * Adding function to check if polygon is convex
    
    * untracking executable
    
    * is inside no convex polygons and lodepng for graphics

--- geocomp.hpp
+++ geocomp.hpp
@@ -151,5 +151,8 @@ bool is_autoint( const Polygon& poly );
 
 //Tarea
 bool is_convex(const Polygon& poly);
+bool is_inside_noconvex(const Polygon& poly, Point pt);
+=======
+
 #endif
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
+
+#include <iostream>
+#include "lodepng.h"
 #include "geocomp.hpp"
+
+using namespace std;
 
 int main()
 {
-  return 0;
-}
+  Polygon poly;
+  poly.push(Vertex(500,300));
+  poly.push(Vertex(700,100));
+  poly.push(Vertex(300,100));
+  poly.push(Vertex(100,300));
+  poly.push(Vertex(300,500));
+  poly.push(Vertex(700,500));
+
+  unsigned width  = 800;
+  unsigned height = 600;
+
+  vector<unsigned char> image;
+
+  for(unsigned y=0; y < height; y +=1)
+  {
+    for(unsigned x = 0; x < width; x +=1)
+    {
+      if(is_inside_noconvex(poly, Point(x,y)))
+      {
+        image.push_back(199);
+        image.push_back(163);
+        image.push_back(199);
+        image.push_back(255);
+        continue;
+      }
+      image.push_back(120);
+      image.push_back(89);
+      image.push_back(219);
+      image.push_back(255);
+    }
+  }
+
+  unsigned error = lodepng::encode("prueba.png", image, width,height);
+  if(error) return 1;
+
